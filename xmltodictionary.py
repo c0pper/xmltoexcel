9ff556af067d5a9bf3df3e3f66266847763b3af9
smarotta <smarotta@expert.ai>
2021-09-16 09:15:34 +0200

    first commit

---
+++ xmltodictionary.py
@@ -0,0 +1,40 @@
+import xmltodict
+import json
+from openpyxl import Workbook, load_workbook
+from openpyxl.utils import get_column_letter
+
+selected_file = input("Copia il nome del report in formato xml: ")
+xmlreport = open(selected_file, "r")
+
+report = json.dumps(xmltodict.parse(xmlreport.read()), indent=4)
+json_report = json.loads(report)
+
+
+precision = []
+recall = []
+f_measure = []
+
+for file in json_report["report"]["files"]["file"]:
+    for p in file["properties"]["property"]:
+        if p["key"] == "EXT_PRECISION":
+            precision.append(round(float(p["value"]), 2))
+        elif p["key"] == "EXT_RECALL":
+            recall.append(round(float(p["value"]), 2))
+        elif p["key"] == "EXT_F1":
+            f_measure.append(round(float(p["value"]), 2))
+
+
+wb = load_workbook("xmlreport.xlsx")
+ws = wb.active
+
+for row in range(1, len(precision)+1):
+    for col in range(1, 4):
+         char = get_column_letter(col)
+         if char == "A":
+             ws[char + str(row)] = precision[row - 1]
+         if char == "B":
+             ws[char + str(row)] = recall[row - 1]
+         if char == "C":
+             ws[char + str(row)] = f_measure[row - 1]
+
+wb.save("xmlreport.xlsx")
